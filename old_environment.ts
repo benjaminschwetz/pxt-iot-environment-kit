3589ae80cab0a647eaa44eb2f47e5f7607096559
BJS (Benjamin Schwetz) <bjs@novozymes.com>
2019-07-07 18:58:38 +0200

    some renaming and writing scope. not much done yet

--- old_environment.ts
+++ old_environment.ts
@@ -3,7 +3,7 @@
 /**
  * Custom blocks
  */
-//% weight=90 color=#ff7a4b icon="\uf0ee" block="Sensors"
+//% weight=90 color=#ff7a4b icon="\uf0ee" block="Octopus"
 namespace Environment {
 
     // keep track of services
